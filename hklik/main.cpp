71567365495b357bea185b9d35c9fab412a5f59d
s20181102934 <s20181102934@admindeimac-38.local>
2019-06-13 11:51:08 +0800

    Initial Commit

---
+++ hklik/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hklik
+//
+//  Created by s20181102934 on 2019/6/13.
+//  Copyright © 2019 tod. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
