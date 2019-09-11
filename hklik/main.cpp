c1ed4e51db8d14f09a24f16bd1c860e52a80f06e
s20181102934 <s20181102934@admindeiMac-38.local>
2019-09-11 11:51:53 +0800

    crazy

--- hklik/main.cpp
+++ hklik/main.cpp
@@ -1,20 +1,44 @@
+
 //
-//  main.cpp
-//  hklik
+//  main.swift
+//  sd
 //
-//  Created by s20181102934 on 2019/6/13.
-//  Copyright © 2019 tod. All rights reserved.
+//  Created by s20181102934 on 2019/9/4.
+//  Copyright © 2019 win. All rights reserved.
 //
 
-#include <iostream>
-#include<fstream>
-using namespace std;
-int main(){
-    ofstream timi("/Users/s20181102934/Desktop/timi.doc ");
-    if(timi.is_open()){
-        timi<<"David is Lv223";
-        timi.close();
-       
+var a:String
+var b:String,c:String
+print("enter username:\n")
+a=readLine()!
+if(a=="admin"){
+    print("enter password:\n")
+    b=readLine()!
+    if(b=="88888888"){
+        print("welcome\n")
+        print("do you want to hold on?\n")
+        c=readLine()!
+        if(c=="yes"){
+            print("it's so great,yu will be warm!")
+        }
+        else{
+            while(c=="no"){
+                print("you need to think twice\n")
+                c=readLine()!
+                if(c=="yes"){
+                    print("yu love u")
+                }
+                
+            }
+        }
     }
-    return 0;
+    else{
+        print("wrong password\n")
+    }
 }
+else{
+    print("username doesnt exist\n")
+}
+
+
+
